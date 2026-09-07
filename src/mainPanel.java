14164a5dd1ccd47efad445b7c4aba836200829bd
Choirunnaswa Alayubi <naswaalayubi@gmail.com>
2026-09-07 22:54:10 +0700

    Add files via upload

---
+++ src/mainPanel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admin;
+
+import java.awt.CardLayout;
+
+/**
+ *
+ * @author Desktop-Putri
+ */
+class mainPanel {
+
+    static void setLayout(CardLayout cardLayout) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void add(PanelPengguna panelPengguna, String panelPengguna0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
